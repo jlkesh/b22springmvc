831ee5e498432fd888080c6d3745224372301daa
pdp <john.lgd65@gmail.com>
2023-03-07 17:45:19 +0500

    go go

--- src/main/resources/templates/main.js
+++ src/main/resources/templates/main.js
@@ -10,44 +10,57 @@ let todos = [
     }
 ]
 
-window.onload = function() {
+window.onload = function () {
     load();
     const myInput = document.querySelector('input[name="inp"]');
     myInput.addEventListener("keydown", (e) => {
-        if (e.keyCode === 13){
-            updateTodo(e.target.id)
+        if (e.keyCode === 13) {
+            updateTodo(e.target.id, e.target.value)
+            console.log(todos)
         }
     });
 };
 
+function updateTodo(id, title) {
+    todos.find(t => t.id === parseInt(id)).title = title;
+}
+
+
 function load() {
     let todosElement = document.getElementById("todos");
-    todos.forEach((todo=>{
-        var newelement = document.createElement('div');
-        newelement.className = 'input-group mb-3';
-        newelement.innerHTML =
-                `<div class="input-group-text">
-                    <input class="form-check-input mt-0" ${todo.done ? 'checked' : ''} type="checkbox" value="">
-                </div>
-                <input type="text" class="form-control" value="${todo.title}" id="${todo.id}" name="inp">
-                <button class="btn btn-danger" type="button" >Delete</button>`;
-        todosElement.appendChild(newelement);
-    }));
+    todos.forEach(todo => {
+        todosElement.appendChild(newTodoElement(todo));
+    });
 }
 
 
 function add() {
     const inputElement = document.getElementById("input");
     const todosElement = document.getElementById("todos");
-    let newDivElement = document.createElement('div');
-    newDivElement.className = 'input-group mb-3';
-    newDivElement.innerHTML = `
-                <div class="input-group-text">
-                    <input class="form-check-input mt-0" type="checkbox" value="">
-                </div>
-                <input type="text" class="form-control" value="${inputElement.value}">
-                <button class="btn btn-danger" type="button" >Delete</button>`
-    todosElement.appendChild(newDivElement)
+    const todo = {"id": new Date().getTime(), "title": inputElement.value};
+    todosElement.appendChild(newTodoElement(todo))
     console.log("123")
 
 }
+
+
+function newTodoElement(todo) {
+    let newelement = document.createElement('div');
+    newelement.className = 'input-group mb-3';
+    newelement.id = "ie-" + todo.id;
+    newelement.innerHTML =
+        `<div class="input-group-text">
+                    <input class="form-check-input mt-0" ${todo.done ? 'checked' : ''} type="checkbox" value="">
+                </div>
+                <input type="text" class="form-control" value="${todo.title}" id="${todo.id}" name="inp">
+                <button class="btn btn-danger" type="button" onclick="deleteTodo(${todo.id})" >Delete</button>`;
+    return newelement;
+}
+
+function deleteTodo(id) {
+    let todos = document.getElementById('todos');
+    let todo = document.getElementById('ie-' + id);
+    todo.remove()
+}
+
+// https://www.javascripttutorial.net/
